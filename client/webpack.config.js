3c29b5d494cae3c1978a883632a3c01f1e5a8117
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-04-02 18:49:32 -0400

    fixed gitignore, added webpack, added indexedDB functionality, began service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,10 +32,6 @@ module.exports = () => {
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
